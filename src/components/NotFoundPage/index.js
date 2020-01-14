400e5fdf17a087881f9d01959bd52883debfa345
blzpedro <phenriqmelo99@gmail.com>
2020-01-14 20:57:44 -0300

    iniciando projeto - week 1

---
+++ src/components/NotFoundPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFoundPage() {
+  return (
+    <>
+        <h1>Não foi possivel encontrar essa rota :(.</h1>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import LoginPage from './components/LoginPage';
+import NotFoundPage from './components/NotFoundPage';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroPage from './components/CadastroPage';
+import Header from './containers/Header';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Header/>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/cadastro" component={CadastroPage} />
+            <Route exact component={NotFoundPage} />
+        </Switch>
+    </BrowserRouter>,   
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
